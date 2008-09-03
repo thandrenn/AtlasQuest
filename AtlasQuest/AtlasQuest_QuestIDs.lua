0c498fa0a9e55581be6e1b231ea59f2d57b069c3
mystery8 <mystery8@8b53c0c3-663e-0410-94af-f901576aafbe>
2008-09-03 19:34:42 +0000

    - Reworked Quest Link code to use localization.

--- AtlasQuest/AtlasQuest_QuestIDs.lua
+++ AtlasQuest/AtlasQuest_QuestIDs.lua
@@ -19,13 +19,27 @@
 	along with AtlasQuest; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+
+This file is for storing quest IDs and Levels.
+
+The Quest IDs are stored in the old/current variable format:
+
+Inst[INST #]Quest[QUEST #]_QuestID
+Inst1Quest1_QuestID
+
+The Quest Levels are stored in the new format:
+AQ_[Quest ID]_Level
+AQ_214_Level
+
+
+Although the quest levels are already stored in the localization files, I
+cannot be 100% certain they are all up to date. Storing them here as well
+for the purpose of quest links helps with that.
+
+
+- Thandrenn
+
 --]]
-
-
--- Storing Quest ID numbers in here for future usage.
-
-
-
 
 ----------------------------------------------
 ---------------- DUNGEONS --------------------
@@ -37,65 +51,46 @@
 --------------- INST1 - Deadmines (VC) ---------------
 
 Inst1Quest1_QuestID = "214"
-Inst1Quest1_QuestLink = "|cffffff00|Hquest:214:17|h[Red Silk Bandanas]|h|r"
-
 Inst1Quest2_QuestID = "168"
-Inst1Quest2_QuestLink = "|cffffff00|Hquest:168:18|h[Collecting Memories]|h|r"
-
 Inst1Quest3_QuestID = "167"
-Inst1Quest3_QuestLink = "|cffffff00|Hquest:167:20|h[Oh Brother. . .]|h|r"
-
 Inst1Quest4_QuestID = "2040"
-Inst1Quest4_QuestLink = "|cffffff00|Hquest:2040:20|h[Underground Assault]|h|r"
-
 Inst1Quest5_QuestID = "166"
-Inst1Quest5_QuestLink = "|cffffff00|Hquest:166:22|h[The Defias Brotherhood]|h|r"
-
 Inst1Quest6_QuestID = "1654"
-Inst1Quest6_QuestLink = "|cffffff00|Hquest:1654:22|h[The Test of Righteousness]|h|r"
-
 Inst1Quest7_QuestID = "373"
-Inst1Quest7_QuestLink = "|cffffff00|Hquest:373:22|h[The Unsent Letter]|h|r"
+
+AQ_214_Level = "17"
+AQ_168_Level = "18"
+AQ_167_Level = "20"
+AQ_2040_Level = "20"
+AQ_166_Level = "22"
+AQ_1654_Level = "22"
+AQ_373_Level = "22"
 
 
 
 --------------- INST2 - Wailing Caverns (WC) ---------------
 
 Inst2Quest1_QuestID = "1486"
-Inst2Quest1_QuestLink = "|cffffff00|Hquest:1486:17|h[Deviate Hides]|h|r"
-
 Inst2Quest2_QuestID = "959"
-Inst2Quest2_QuestLink = "|cffffff00|Hquest:959:18|h[Trouble at the Docks]|h|r"
-
 Inst2Quest3_QuestID = "1491"
-Inst2Quest3_QuestLink = "|cffffff00|Hquest:1491:18|h[Smart Drinks]|h|r"
-
 Inst2Quest4_QuestID = "1487"
-Inst2Quest4_QuestLink = "|cffffff00|Hquest:1487:21|h[Deviate Eradication]|h|r"
-
 Inst2Quest5_QuestID = "6981"
-Inst2Quest5_QuestLink = "|cffffff00|Hquest:6981:26|h[The Glowing Shard]|h|r"
 
 Inst2Quest1_HORDE_QuestID = "1486"
-Inst2Quest1_HORDE_QuestLink = "|cffffff00|Hquest:1486:17|h[Deviate Hides]|h|r"
-
 Inst2Quest2_HORDE_QuestID = "959"
-Inst2Quest2_HORDE_QuestLink = "|cffffff00|Hquest:959:18|h[Trouble at the Docks]|h|r"
-
 Inst2Quest3_HORDE_QuestID = "962"
-Inst2Quest3_HORDE_QuestLink = "|cffffff00|Hquest:962:18|h[Serpentbloom]|h|r"
-
 Inst2Quest4_HORDE_QuestID = "1491"
-Inst2Quest4_HORDE_QuestLink = "|cffffff00|Hquest:1491:18|h[Smart Drinks]|h|r"
-
 Inst2Quest5_HORDE_QuestID = "1487"
-Inst2Quest5_HORDE_QuestLink = "|cffffff00|Hquest:1487:21|h[Deviate Eradication]|h|r"
-
 Inst2Quest6_HORDE_QuestID = "914"
-Inst2Quest6_HORDE_QuestLink = "|cffffff00|Hquest:914:22|h[Leaders of the Fang]|h|r"
-
 Inst2Quest7_HORDE_QuestID = "6981"
-Inst2Quest7_HORDE_QuestLink = "|cffffff00|Hquest:6981:26|h[The Glowing Shard]|h|r"
+
+AQ_1486_Level = "17"
+AQ_959_Level = "18"
+AQ_1491_Level = "18"
+AQ_1487_Level = "21"
+AQ_6981_Level = "26"
+AQ_962_Level = "18"
+AQ_914_Level = "22"
 
 
 
