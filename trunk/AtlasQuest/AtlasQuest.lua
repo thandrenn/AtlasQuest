04c923aeb343a175f281f54c88e9861268b36d10
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2011-03-22 19:05:44 +0000

    - Releasing 4.6.2

--- trunk/AtlasQuest/AtlasQuest.lua
+++ trunk/AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "107"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.6.2 BETA";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.6.2";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.6.2 BETA",
+  ["Version"] =  "4.6.2",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
