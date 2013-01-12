e25ff62e4af268e4d8e4097113ed624879575168
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2013-01-12 19:31:12 +0000

    - Fixed missing End of File in DE locale - Releasing 4.8.2

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "137"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.2 BETA";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.2";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.8.2 BETA",
+  ["Version"] =  "4.8.2",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
