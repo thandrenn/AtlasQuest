0436b163da3a141bb9f428418413d19d6d686600
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2013-07-26 04:35:09 +0000

    - Updated EN with new quests in Throne of Thunder.

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "144"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.4";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.5 BETA";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.8.4",
+  ["Version"] =  "4.8.5 BETA",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
