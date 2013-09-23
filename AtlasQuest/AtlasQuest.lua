512870bd2a397797b5d524d67824fee90af1651a
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2013-09-23 19:35:14 +0000

    - Fixed: Starting location of Stratholme: Liquid Gold - Fixed: Starting location of Shattered Halls: Advancing the Campaign (Horde)

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "145"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.6";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.7 BETA";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.8.6",
+  ["Version"] =  "4.8.7 BETA",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
