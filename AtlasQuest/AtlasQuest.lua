ae6ec64b9bb692deb77ea4793d353076613241b6
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2013-11-26 06:59:44 +0000

    - Releasing 4.8.7
    yay!  :)

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "145"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.7 BETA";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.8.7";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.8.7 BETA",
+  ["Version"] =  "4.8.7",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
