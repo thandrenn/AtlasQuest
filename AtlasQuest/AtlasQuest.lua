b0e0f28ee7ec906ff6fe368a17b880be06289695
mystery8@gmail.com <mystery8@gmail.com@8b53c0c3-663e-0410-94af-f901576aafbe>
2014-12-03 10:51:16 +0000

    - Shadowmoon Burial Grounds added - Upper Blackrock Spire added - Highmaul added
    All Draenor quests should now be in.  Let me know if any are missing.

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -65,14 +65,14 @@ AQINSTANZ = 1; -- currently shown instance-pic (see AtlasQuest_Instanzen.lua)
 AQINSTATM = ""; -- variable to check whether AQINSTANZ has changed (see function AtlasQuestSetTextandButtons())
 
 -- Sets the max number of instances and quests to check for. 
-local AQMAXINSTANCES = "156"
+local AQMAXINSTANCES = "155"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.9.1 ALPHA";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.9.1 BETA";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.9.1 ALPHA",
+  ["Version"] =  "4.9.1 BETA",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
