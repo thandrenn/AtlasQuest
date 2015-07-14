65f407b61ea028dc815f891825c4f1469c0414f7
thandrenn <thandrenn@users.noreply.github.com>
2015-07-14 00:17:31 -0400

    - Releasing 4.9.4.  Hooray!

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -69,10 +69,10 @@ local AQMAXINSTANCES = "156"
 local AQMAXQUESTS = "22"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.9.4 BETA";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.9.4";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.9.4 BETA",
+  ["Version"] =  "4.9.4",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
