206fe216feea5b6898ea64d22b8363e98e66643e
ChemicalFighter <chemicalfighter@gmx.de>
2016-08-20 22:35:37 +0200

    Dede Legion not realy finished rest when Addon is Start+fixed a small mistaces

--- AtlasQuest/Locale/legion-deDE.lua
+++ AtlasQuest/Locale/legion-deDE.lua
@@ -70,34 +70,34 @@ Inst157QAA = "3 Quests"
 Inst157QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst157Quest1 = "1. Violet Hold"
+Inst157Quest1 = "1. Violette Festung"
 Inst157Quest1_Aim = "Defeat the leader of the Assault on Violet Hold."
-Inst157Quest1_Location = "Violet Hold (Auto-accept)"
+Inst157Quest1_Location = "Violet Hold (Automatische Annahme)"
 Inst157Quest1_Note = ""
 Inst157Quest1_Prequest = "Nein"
 Inst157Quest1_Folgequest = "Nein"
 --
-Inst157Quest1name1 = "Violet Hold Contraband Locker"
+Inst157Quest1name1 = "Schmuggelwarenspind der Violetten Festung"
 
 --Quest 2 Alliance
-Inst157Quest2 = "2. Violet Hold (Heroic)"
-Inst157Quest2_Aim = "Defeat the leader of the Assault on Violet Hold on Heroic difficulty or higher."
-Inst157Quest2_Location = "Violet Hold (Auto-accept)"
+Inst157Quest2 = "2. Violette Festung (Heroisch)"
+Inst157Quest2_Aim = "Defeat the leader of the Assault on Violet Hold auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst157Quest2_Location = Inst157Quest1_Location
 Inst157Quest2_Note = ""
 Inst157Quest2_Prequest = "Nein"
 Inst157Quest2_Folgequest = "Nein"
 --
-Inst157Quest2name1 = "Violet Hold Contraband Locker"
+Inst157Quest2name1 = Inst157Quest1name1
 
 --Quest 3 Alliance
-Inst157Quest3 = "3. Violet Hold (Mythic)"
-Inst157Quest3_Aim = "Defeat the leader of the Assault on Violet Hold on Mythic difficulty."
-Inst157Quest3_Location = "Violet Hold (Auto-accept)"
+Inst157Quest3 = "3. Violette Festung (Mythisch)"
+Inst157Quest3_Aim = "Defeat the leader of the Assault on Violet Hold auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst157Quest3_Location = Inst157Quest1_Location
 Inst157Quest3_Note = ""
 Inst157Quest3_Prequest = "Nein"
 Inst157Quest3_Folgequest = "Nein"
 --
-Inst157Quest3name1 = "Violet Hold Contraband Locker"
+Inst157Quest3name1 = Inst157Quest1name1
 
 
 --Quest 1 Horde
@@ -118,7 +118,7 @@ Inst157Quest2_HORDE_Note = Inst157Quest2_Note
 Inst157Quest2_HORDE_Prequest = "Nein"
 Inst157Quest2_HORDE_Folgequest = "Nein"
 --
-Inst157Quest2name1_HORDE = Inst157Quest2name1
+Inst157Quest2name1_HORDE = Inst157Quest1name1
 
 --Quest 3 Horde
 Inst157Quest3_HORDE = Inst157Quest3
@@ -128,20 +128,20 @@ Inst157Quest3_HORDE_Note = Inst157Quest3_Note
 Inst157Quest3_HORDE_Prequest = "Nein"
 Inst157Quest3_HORDE_Folgequest = "Nein"
 --
-Inst157Quest3name1_HORDE = Inst157Quest3name1
+Inst157Quest3name1_HORDE = Inst157Quest1name1
 
 
 
 --------------- INST158 - Black Rook Hold ---------------
 
-Inst158Caption = "Black Rook Hold"
+Inst158Caption = "Die Rabenwehr"
 Inst158QAA = "3 Quests"
 Inst158QAH = "3 Quests"
 
 --Quest 1 Alliance
 Inst158Quest1 = "1. The Lord of Black Rook Hold"
 Inst158Quest1_Aim = "Defeat Lord Kur'talos Ravencrest in Black Rook Hold."
-Inst158Quest1_Location = "Black Rook Hold (Auto-accept)"
+Inst158Quest1_Location = "Black Rook Hold (Automatische Annahme)"
 Inst158Quest1_Note = ""
 Inst158Quest1_Prequest = "Nein"
 Inst158Quest1_Folgequest = "Nein"
@@ -150,8 +150,8 @@ Inst158Quest1name1 = "Haunted Ravencrest Keepsake"
 
 --Quest 2 Alliance
 Inst158Quest2 = "2. The Lord of Black Rook Hold (Heroic)"
-Inst158Quest2_Aim = "Defeat Kur'talos Ravencrest in the Black Rook Hold on Heroic difficulty or higher."
-Inst158Quest2_Location = "Black Rook Hold (Auto-accept)"
+Inst158Quest2_Aim = "Defeat Kur'talos Ravencrest in the Black Rook Hold auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst158Quest2_Location = Inst158Quest1_Location
 Inst158Quest2_Note = ""
 Inst158Quest2_Prequest = "Nein"
 Inst158Quest2_Folgequest = "Nein"
@@ -160,8 +160,8 @@ Inst158Quest2name1 = "Haunted Ravencrest Keepsake"
 
 --Quest 3 Alliance
 Inst158Quest3 = "3. The Lord of Black Rook Hold (Mythic)"
-Inst158Quest3_Aim = "Defeat Kur'talos Ravencrest in the Black Rook Hold on Mythic difficulty."
-Inst158Quest3_Location = "Black Rook Hold (Auto-accept)"
+Inst158Quest3_Aim = "Defeat Kur'talos Ravencrest in the Black Rook Hold auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst158Quest3_Location = Inst158Quest1_Location
 Inst158Quest3_Note = ""
 Inst158Quest3_Prequest = "Nein"
 Inst158Quest3_Folgequest = "Nein"
@@ -203,14 +203,14 @@ Inst158Quest3name1_HORDE = Inst158Quest3name1
 
 --------------- INST159 - Court of Stars ---------------
 
-Inst159Caption = "Court of Stars"
+Inst159Caption = "Der Hof der Sterne"
 Inst159QAA = "3 Quests"
 Inst159QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst159Quest1 = "1. Court of Stars"
-Inst159Quest1_Aim = "Defeat Advisor Melandrus in Court of Stars."
-Inst159Quest1_Location = "Court of Stars (Auto-accept)"
+Inst159Quest1 = "1. Der Hof der Sterne"
+Inst159Quest1_Aim = "Bezwingt Berater Melandrus im Hof der Sterne."
+Inst159Quest1_Location = "Der Hof der Sterne (Automatische Annahme)"
 Inst159Quest1_Note = ""
 Inst159Quest1_Prequest = "Nein"
 Inst159Quest1_Folgequest = "Nein"
@@ -218,9 +218,9 @@ Inst159Quest1_Folgequest = "Nein"
 Inst159Quest1name1 = "Nightborne Rucksack"
 
 --Quest 2 Alliance
-Inst159Quest2 = "2. Court of Stars (Heroic)"
-Inst159Quest2_Aim = "Defeat Advisor Melandrus in Court of Stars on Heroic difficulty or higher."
-Inst159Quest2_Location = "Court of Stars (Auto-accept)"
+Inst159Quest2 = "2. Der Hof der Sterne (Heroisch)"
+Inst159Quest2_Aim = "Bezwingt Berater Melandrus im Hof der Sterne auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst159Quest2_Location = Inst159Quest1_Note
 Inst159Quest2_Note = ""
 Inst159Quest2_Prequest = "Nein"
 Inst159Quest2_Folgequest = "Nein"
@@ -228,9 +228,9 @@ Inst159Quest2_Folgequest = "Nein"
 Inst159Quest2name1 = "Nightborne Rucksack"
 
 --Quest 3 Alliance
-Inst159Quest3 = "3. Court of Stars (Mythic)"
-Inst159Quest3_Aim = "Defeat Advisor Melandrus in Court of Stars on Mythic difficulty."
-Inst159Quest3_Location = "Court of Stars (Auto-accept)"
+Inst159Quest3 = "3. Der Hof der Sterne (Mythisch)"
+Inst159Quest3_Aim = "Bezwingt Berater Melandrus im Hof der Sterne auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst159Quest3_Location = Inst159Quest1_Note
 Inst159Quest3_Note = ""
 Inst159Quest3_Prequest = "Nein"
 Inst159Quest3_Folgequest = "Nein"
@@ -272,14 +272,14 @@ Inst159Quest3name1_HORDE = Inst159Quest3name1
 
 --------------- INST160 - Darkheart Thicket ---------------
 
-Inst160Caption = "Darkheart Thicket"
+Inst160Caption = "Das Finsterherzdickicht"
 Inst160QAA = "3 Quests"
 Inst160QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst160Quest1 = "1. Darkheart Thicket"
-Inst160Quest1_Aim = "Defeat the Shade of Xavius in Darkheart Thicket."
-Inst160Quest1_Location = "Darkheart Thicket (Auto-accept)"
+Inst160Quest1 = "1. Finsterherzdickicht"
+Inst160Quest1_Aim = "Bezwingt den Schemen von Xavius im Finsterherzdickicht."
+Inst160Quest1_Location = "Darkheart Thicket (Automatische Annahme)"
 Inst160Quest1_Note = ""
 Inst160Quest1_Prequest = "Nein"
 Inst160Quest1_Folgequest = "Nein"
@@ -287,9 +287,9 @@ Inst160Quest1_Folgequest = "Nein"
 Inst160Quest1name1 = "Despoiled Keeper's Cache"
 
 --Quest 2 Alliance
-Inst160Quest2 = "2. Darkheart Thicket (Heroic)"
-Inst160Quest2_Aim = "Defeat the Shade of Xavius in Darkheart Thicket on Heroic difficulty or higher."
-Inst160Quest2_Location = "Darkheart Thicket (Auto-accept)"
+Inst160Quest2 = "2. Finsterherzdickicht (Heroisch)"
+Inst160Quest2_Aim = "Bezwingt den Schemen von Xavius im Finsterherzdickicht auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst160Quest2_Location = Inst160Quest1_Location
 Inst160Quest2_Note = ""
 Inst160Quest2_Prequest = "Nein"
 Inst160Quest2_Folgequest = "Nein"
@@ -297,9 +297,9 @@ Inst160Quest2_Folgequest = "Nein"
 Inst160Quest2name1 = "Despoiled Keeper's Cache"
 
 --Quest 3 Alliance
-Inst160Quest3 = "3. Darkheart Thicket (Mythic)"
-Inst160Quest3_Aim = "Defeat the Shade of Xavius in Darkheart Thicket on Mythic difficulty."
-Inst160Quest3_Location = "Darkheart Thicket (Auto-accept)"
+Inst160Quest3 = "3. Finsterherzdickicht (Mythisch)"
+Inst160Quest3_Aim = "Bezwingt den Schemen von Xavius im Finsterherzdickicht auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst160Quest3_Location = Inst160Quest1_Location
 Inst160Quest3_Note = ""
 Inst160Quest3_Prequest = "Nein"
 Inst160Quest3_Folgequest = "Nein"
@@ -350,14 +350,14 @@ Inst161QAH = "6 Quests"
 
 --------------- INST162 - Halls of Valor ---------------
 
-Inst162Caption = "Halls of Valor"
+Inst162Caption = "Die Hallen der Tapferkeit"
 Inst162QAA = "3 Quests"
 Inst162QAH = "3 Quests"
 
 --Quest 1 Alliance
 Inst162Quest1 = "1. Odyn's Challenge"
-Inst162Quest1_Aim = "Defeat Odyn in the Halls of Valor on Normal difficulty."
-Inst162Quest1_Location = "Halls of Valor (Auto-accept)"
+Inst162Quest1_Aim = "Bezwingt Odyn in den Hallen der Tapferkeit."
+Inst162Quest1_Location = "Halls of Valor (Automatische Annahme)"
 Inst162Quest1_Note = ""
 Inst162Quest1_Prequest = "Nein"
 Inst162Quest1_Folgequest = "Nein"
@@ -366,8 +366,8 @@ Inst162Quest1name1 = "Challenger's Spoils"
 
 --Quest 2 Alliance
 Inst162Quest2 = "2. Odyn's Challenge (Heroic)"
-Inst162Quest2_Aim = "Defeat Odyn in the Halls of Valor on Heroic difficulty or higher."
-Inst162Quest2_Location = "Halls of Valor (Auto-accept)"
+Inst162Quest2_Aim = "Bezwingt Odyn in den Hallen der Tapferkeit auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst162Quest2_Location = Inst162Quest1_Location
 Inst162Quest2_Note = ""
 Inst162Quest2_Prequest = "Nein"
 Inst162Quest2_Folgequest = "Nein"
@@ -376,8 +376,8 @@ Inst162Quest2name1 = "Challenger's Spoils"
 
 --Quest 3 Alliance
 Inst162Quest3 = "3. Odyn's Challenge (Mythic)"
-Inst162Quest3_Aim = "Defeat Odyn in the Halls of Valor on Mythic difficulty."
-Inst162Quest3_Location = "Halls of Valor (Auto-accept)"
+Inst162Quest3_Aim = "Bezwingt Odyn in den Hallen der Tapferkeit auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst162Quest3_Location = Inst162Quest1_Location
 Inst162Quest3_Note = ""
 Inst162Quest3_Prequest = "Nein"
 Inst162Quest3_Folgequest = "Nein"
@@ -419,14 +419,14 @@ Inst162Quest3name1_HORDE = Inst162Quest3name1
 
 --------------- INST163 - Maw of Souls ---------------
 
-Inst163Caption = "Maw of Souls"
+Inst163Caption = "Der Seelenschlund"
 Inst163QAA = "3 Quests"
 Inst163QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst163Quest1 = "1. Maw of Souls"
-Inst163Quest1_Aim = "Defeat Helya in Maw of Souls."
-Inst163Quest1_Location = "Maw of Souls (Auto-accept)"
+Inst163Quest1 = "1. Der Seelenschlund"
+Inst163Quest1_Aim = "Bezwingt Helya im Seelenschlund."
+Inst163Quest1_Location = "Maw of Souls (Automatische Annahme)"
 Inst163Quest1_Note = ""
 Inst163Quest1_Prequest = "Nein"
 Inst163Quest1_Folgequest = "Nein"
@@ -434,9 +434,9 @@ Inst163Quest1_Folgequest = "Nein"
 Inst163Quest1name1 = "Seaweed-Encrusted Satchel"
 
 --Quest 2 Alliance
-Inst163Quest2 = "2. Maw of Souls (Heroic)"
-Inst163Quest2_Aim = "Defeat Helya in Maw of Souls on Heroic difficulty or higher."
-Inst163Quest2_Location = "Maw of Souls (Auto-accept)"
+Inst163Quest2 = "2. Der Seelenschlund (Heroisch)"
+Inst163Quest2_Aim = "Bezwingt Helya im Seelenschlund auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst163Quest2_Location = Inst163Quest1_Location
 Inst163Quest2_Note = ""
 Inst163Quest2_Prequest = "Nein"
 Inst163Quest2_Folgequest = "Nein"
@@ -444,9 +444,9 @@ Inst163Quest2_Folgequest = "Nein"
 Inst163Quest2name1 = "Seaweed-Encrusted Satchel"
 
 --Quest 3 Alliance
-Inst163Quest3 = "3. Maw of Souls (Mythic)"
-Inst163Quest3_Aim = "Defeat Helya in Maw of Souls on Mythic difficulty."
-Inst163Quest3_Location = "Maw of Souls (Auto-accept)"
+Inst163Quest3 = "3. Der Seelenschlund (Mythisch)"
+Inst163Quest3_Aim = "Bezwingt Helya im Seelenschlund auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst163Quest3_Location = Inst163Quest1_Location
 Inst163Quest3_Note = ""
 Inst163Quest3_Prequest = "Nein"
 Inst163Quest3_Folgequest = "Nein"
@@ -495,32 +495,32 @@ Inst164QAH = "3 Quests"
 --Quest 1 Alliance
 Inst164Quest1 = "1. Neltharion's Lair"
 Inst164Quest1_Aim = "Defeat Dargrul in Neltharion's Lair."
-Inst164Quest1_Location = "Neltharion's Lair (Auto-accept)"
+Inst164Quest1_Location = "Neltharion's Lair (Automatische Annahme)"
 Inst164Quest1_Note = ""
 Inst164Quest1_Prequest = "Nein"
 Inst164Quest1_Folgequest = "Nein"
 --
-Inst164Quest1name1 = "Cache of the Black Dragon"
+Inst164Quest1name1 = "Truhe des schwarzen Großdrachen"
 
 --Quest 2 Alliance
 Inst164Quest2 = "2. Neltharion's Lair (Heroic)"
-Inst164Quest2_Aim = "Defeat Dargrul in Neltharion's Lair on Heroic difficulty or higher."
-Inst164Quest2_Location = "Neltharion's Lair (Auto-accept)"
+Inst164Quest2_Aim = "Defeat Dargrul in Neltharion's Lair auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst164Quest2_Location = Inst164Quest1_Location
 Inst164Quest2_Note = ""
 Inst164Quest2_Prequest = "Nein"
 Inst164Quest2_Folgequest = "Nein"
 --
-Inst164Quest2name1 = "Cache of the Black Dragon"
+Inst164Quest2name1 = Inst164Quest1name1
 
 --Quest 3 Alliance
 Inst164Quest3 = "3. Neltharion's Lair (Mythic)"
-Inst164Quest3_Aim = "Defeat Dargrul in Neltharion's Lair on Mythic difficulty."
-Inst164Quest3_Location = "Neltharion's Lair (Auto-accept)"
+Inst164Quest3_Aim = "Defeat Dargrul in Neltharion's Lair auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst164Quest3_Location = Inst164Quest1_Location
 Inst164Quest3_Note = ""
 Inst164Quest3_Prequest = "Nein"
 Inst164Quest3_Folgequest = "Nein"
 --
-Inst164Quest3name1 = "Cache of the Black Dragon"
+Inst164Quest3name1 = Inst164Quest1name1
 
 
 --Quest 1 Horde
@@ -541,7 +541,7 @@ Inst164Quest2_HORDE_Note = Inst164Quest2_Note
 Inst164Quest2_HORDE_Prequest = "Nein"
 Inst164Quest2_HORDE_Folgequest = "Nein"
 --
-Inst164Quest2name1_HORDE = Inst164Quest2name1
+Inst164Quest2name1_HORDE = Inst164Quest1name1
 
 --Quest 3 Horde
 Inst164Quest3_HORDE = Inst164Quest3
@@ -551,20 +551,20 @@ Inst164Quest3_HORDE_Note = Inst164Quest3_Note
 Inst164Quest3_HORDE_Prequest = "Nein"
 Inst164Quest3_HORDE_Folgequest = "Nein"
 --
-Inst164Quest3name1_HORDE = Inst164Quest3name1
+Inst164Quest3name1_HORDE = Inst164Quest1name1
 
 
 
 --------------- INST165 - The Arcway ---------------
 
-Inst165Caption = "The Arcway"
+Inst165Caption = "Der Arkus"
 Inst165QAA = "3 Quests"
 Inst165QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst165Quest1 = "1. The Arcway"
-Inst165Quest1_Aim = "Defeat Advisor Vandros in The Arcway."
-Inst165Quest1_Location = "The Arcway (Auto-accept)"
+Inst165Quest1 = "1. Der Arkus"
+Inst165Quest1_Aim = "Bezwingt Berater Vandros im Arkus."
+Inst165Quest1_Location = "The Arcway (Automatische Annahme)"
 Inst165Quest1_Note = ""
 Inst165Quest1_Prequest = "Nein"
 Inst165Quest1_Folgequest = "Nein"
@@ -572,9 +572,9 @@ Inst165Quest1_Folgequest = "Nein"
 Inst165Quest1name1 = "Unmarked Suramar Vault Crate"
 
 --Quest 2 Alliance
-Inst165Quest2 = "2. The Arcway (Heroic)"
-Inst165Quest2_Aim = "Defeat Advisor Vandros in The Arcway on Heroic difficulty or higher."
-Inst165Quest2_Location = "The Arcway (Auto-accept)"
+Inst165Quest2 = "2. Der Arkus (Heroisch)"
+Inst165Quest2_Aim = "Bezwingt Berater Vandros im Arkus auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst165Quest2_Location = Inst165Quest1_Location
 Inst165Quest2_Note = ""
 Inst165Quest2_Prequest = "Nein"
 Inst165Quest2_Folgequest = "Neine"
@@ -582,9 +582,9 @@ Inst165Quest2_Folgequest = "Neine"
 Inst165Quest2name1 = "Unmarked Suramar Vault Crate"
 
 --Quest 3 Alliance
-Inst165Quest3 = "3. The Arcway (Mythic)"
-Inst165Quest3_Aim = "Defeat Advisor Vandros in The Arcway on Mythic difficulty."
-Inst165Quest3_Location = "The Arcway (Auto-accept)"
+Inst165Quest3 = "3. Der Arkus (Mythisch)"
+Inst165Quest3_Aim = "Bezwingt Berater Vandros im Arkus auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst165Quest3_Location = Inst165Quest1_Location
 Inst165Quest3_Note = ""
 Inst165Quest3_Prequest = "Nein"
 Inst165Quest3_Folgequest = "Nein"
@@ -626,14 +626,14 @@ Inst165Quest3name1_HORDE = Inst165Quest3name1
 
 --------------- INST166 - Vault of the Wardens ---------------
 
-Inst166Caption = "Vault of the Wardens"
+Inst166Caption = "Das Verlies der Wächterinnen"
 Inst166QAA = "3 Quests"
 Inst166QAH = "3 Quests"
 
 --Quest 1 Alliance
-Inst166Quest1 = "1. Vault of the Wardens"
-Inst166Quest1_Aim = "Defeat Cordana Felsong in Vault of the Wardens."
-Inst166Quest1_Location = "Vault of the Wardens (Auto-accept)"
+Inst166Quest1 = "1. Das Verlies der Wächterinnen"
+Inst166Quest1_Aim = "Bezwingt Cordana Teufelsang im Verlies der Wächterinnen."
+Inst166Quest1_Location = "Das Verlies der Wächterinnen (Automatische Annahme)"
 Inst166Quest1_Note = ""
 Inst166Quest1_Prequest = "Nein"
 Inst166Quest1_Folgequest = "Nein"
@@ -641,9 +641,9 @@ Inst166Quest1_Folgequest = "Nein"
 Inst166Quest1name1 = "Bag of Confiscated Materials"
 
 --Quest 2 Alliance
-Inst166Quest2 = "2. Vault of the Wardens (Heroic)"
-Inst166Quest2_Aim = "Defeat Cordana Felsong in Vault of the Wardens on Heroic difficulty or higher."
-Inst166Quest2_Location = "Vault of the Wardens (Auto-accept)"
+Inst166Quest2 = "2. Das Verlies der Wächterinnen (Heroisch)"
+Inst166Quest2_Aim = "Bezwingt Cordana Teufelsang im Verlies der Wächterinnen auf dem Schwierigkeitsgrad 'Heroisch' oder höher."
+Inst166Quest2_Location = Inst166Quest1_Location
 Inst166Quest2_Note = ""
 Inst166Quest2_Prequest = "Nein"
 Inst166Quest2_Folgequest = "Nein"
@@ -651,9 +651,9 @@ Inst166Quest2_Folgequest = "Nein"
 Inst166Quest2name1 = "Bag of Confiscated Materials"
 
 --Quest 3 Alliance
-Inst166Quest3 = "3. Vault of the Wardens (Mythic)"
-Inst166Quest3_Aim = "Defeat Cordana Felsong in Vault of the Wardens on Mythic difficulty."
-Inst166Quest3_Location = "Vault of the Wardens (Auto-accept)"
+Inst166Quest3 = "3. Das Verlies der Wächterinnen (Mythisch)"
+Inst166Quest3_Aim = "Bezwingt Cordana Teufelsang im Verlies der Wächterinnen auf dem Schwierigkeitsgrad 'Mythisch'."
+Inst166Quest3_Location = Inst166Quest1_Location
 Inst166Quest3_Note = ""
 Inst166Quest3_Prequest = "Nein"
 Inst166Quest3_Folgequest = "Nein"
