dc9e6e371332ca100ca1c94455e6c2998ef18cf4
thandrenn <thandrenn@users.noreply.github.com>
2018-01-28 03:13:44 -0500

    - Releasing Version 4.10.41

--- AtlasQuest/AtlasQuest.lua
+++ AtlasQuest/AtlasQuest.lua
@@ -67,10 +67,10 @@ local AQMAXINSTANCES = "174"
 local AQMAXQUESTS = "23"
 
 -- Set title for AtlasQuest side panel
-ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.10.40";
+ATLASQUEST_VERSION = ""..BLUE.."AtlasQuest 4.10.41";
 
 local AtlasQuest_Defaults = {
-  ["Version"] =  "4.10.40",
+  ["Version"] =  "4.10.41",
   [UnitName("player")] = {
     ["ShownSide"] = "Left",
     ["AtlasAutoShow"] = 1,
